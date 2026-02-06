cbdfdd70a75aa1b00bda3845bef5465d9ab1b45f
Jose <jcuello@gmail.com>
2026-02-05 20:11:01 -0500

    Adds assets and new prefix for routes.

--- main.go
+++ main.go
@@ -8,7 +8,13 @@ func main() {
 	serveMux := http.ServeMux{}
 	server := http.Server{}
 
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	serveMux.HandleFunc("/healthz", func(resp http.ResponseWriter, request *http.Request) {
+		resp.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		resp.WriteHeader(http.StatusOK)
+		resp.Write([]byte("OK"))
+
+	})
 
 	server.Handler = &serveMux
 	server.Addr = ":8080"
